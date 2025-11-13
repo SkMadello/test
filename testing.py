577f8033d067c1af0417408becd84b6a5a34d04d
SkMadello <saravana.kumar@madello.com>
2025-11-13 12:59:50 +0530

    cicd_setup

--- testing.py
+++ testing.py
@@ -1,1 +1,9 @@
-print("Hi sk, how are you?")
+import streamlit as st
+
+st.title("🚀 CI/CD Test App")
+st.write("This is a simple Streamlit app deployed using Docker + GitHub Actions!")
+st.write("If you see this page, your CI/CD pipeline was successful 🎉")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.success(f"Hello, {name} 👋 Welcome to CI/CD pipeline testing!")
